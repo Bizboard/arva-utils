4c4c4aba1d349ee81b67b90bf5aca40388c66403
Tom Clement <mr@tomclement.nl>
2015-06-03 14:12:29 +0200

    Added bundle-sfx compatible image loader for SystemJS

--- config.js
+++ config.js
@@ -10,13 +10,21 @@ System.config({
 
 System.config({
   "map": {
+    "fs": "github:jspm/nodelibs-fs@0.1.2",
     "lodash": "npm:lodash@3.9.3",
+    "path": "github:jspm/nodelibs-path@0.1.0",
     "traceur": "github:jmcriffey/bower-traceur@0.0.88",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:lodash@3.9.3": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
---
+++ src/ImageLoader.js
@@ -0,0 +1,35 @@
+/**
+ * SystemJS Image Loader that embeds images as data URIs inside the application bundle.
+ *
+ * Created by tom on 03/06/15.
+ */
+
+var fs = require('fs');
+var path = require('path');
+
+exports.build = true;
+
+exports.fetch = function (load) {
+    return new Promise(function (resolve, reject) {
+        var absolutePath = load.address.substr('file:'.length);
+        fs.readFile(absolutePath, function (error, data) {
+            if (error) { reject(error); }
+
+            var extension = path.extname(absolutePath).replace('.', '').toLowerCase();
+
+            /* SVG images don't need to be base64 encoded */
+            var isSvg = extension === 'svg';
+            var imgdata = isSvg ? data : data.toString('base64');
+
+            var dataType = 'image/' + extension;
+            var charset = ';charset=utf-8';
+            var encoding = isSvg ? '' : ';base64';
+
+            var uri = 'data:' + dataType + charset + encoding + ',' + imgdata;
+
+            //console.log(uri);
+
+            resolve('module.exports = "' + uri + '"');
+        });
+    });
+};
