db13e37d54c756196df6aa90773078fa38650cdf
Hans van den Akker <hans@bizboard.nl>
2015-06-04 14:04:36 +0200

    supporting exports of SVG properly.

--- src/ImageLoader.js
+++ src/ImageLoader.js
@@ -25,11 +25,16 @@ exports.fetch = function (load) {
             var charset = ';charset=utf-8';
             var encoding = isSvg ? '' : ';base64';
 
-            var uri = 'data:' + dataType + charset + encoding + ',' + imgdata;
+            if (isSvg) {
 
-            //console.log(uri);
-
-            resolve('module.exports = "' + uri + '"');
+                data = data.toString().replace(/(\r\n|\n|\r)/gm, '');
+                //console.log(data);
+                resolve('module.exports = \'' + data + '\';');
+            }
+            else {
+                var uri = 'data:' + dataType + charset + encoding + ',' + imgdata;
+                resolve('module.exports = "' + uri + '";');
+            }
         });
     });
 };
