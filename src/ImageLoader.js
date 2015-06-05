d0494b04d6875678cc00771bf3ec877c696367f5
Tom Clement <mr@tomclement.nl>
2015-06-05 11:15:56 +0200

    Reverting until we have solution in place for Hans' inject-into-DOM use case

--- src/ImageLoader.js
+++ src/ImageLoader.js
@@ -21,14 +21,20 @@ exports.fetch = function (load) {
             var isSvg = extension === 'svg';
             var imgdata = isSvg ? data : data.toString('base64');
 
-            var dataType = 'image/' + isSvg ? 'svg+xml' : extension;
+            var dataType = 'image/' + extension;
             var charset = ';charset=utf-8';
             var encoding = isSvg ? '' : ';base64';
 
-            if (isSvg) { data = data.toString().replace(/(\r\n|\n|\r)/gm, ''); }
+            if (isSvg) {
 
-            var uri = 'data:' + dataType + charset + encoding + ',' + imgdata;
-            resolve('module.exports = "' + uri + '";');
+                data = data.toString().replace(/(\r\n|\n|\r)/gm, '');
+                //console.log(data);
+                resolve('module.exports = \'' + data + '\';');
+            }
+            else {
+                var uri = 'data:' + dataType + charset + encoding + ',' + imgdata;
+                resolve('module.exports = "' + uri + '";');
+            }
         });
     });
 };
