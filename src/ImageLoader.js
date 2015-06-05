52e26f204dca6a815479660be310aafa7086e666
Tom Clement <mr@tomclement.nl>
2015-06-05 12:59:24 +0200

    Fixed SVG loading as data URI

--- src/ImageLoader.js
+++ src/ImageLoader.js
@@ -18,23 +18,15 @@ exports.fetch = function (load) {
             var extension = path.extname(absolutePath).replace('.', '').toLowerCase();
 
             /* SVG images don't need to be base64 encoded */
-            var isSvg = extension === 'svg';
-            var imgdata = isSvg ? data : data.toString('base64');
+            var isSvg = (extension === 'svg');
+            var imgdata = isSvg ? data.toString().replace(/(\r\n|\r|\n)/gm, '') : data.toString('base64');
 
-            var dataType = 'image/' + extension;
+            var dataType = 'image/' + (isSvg ? 'svg+xml' : extension);
             var charset = ';charset=utf-8';
             var encoding = isSvg ? '' : ';base64';
 
-            if (isSvg) {
-
-                data = data.toString().replace(/(\r\n|\n|\r)/gm, '');
-                //console.log(data);
-                resolve('module.exports = \'' + data + '\';');
-            }
-            else {
-                var uri = 'data:' + dataType + charset + encoding + ',' + imgdata;
-                resolve('module.exports = "' + uri + '";');
-            }
+            var uri = 'data:' + dataType + charset + encoding + ',' + imgdata;
+            resolve('module.exports = \'' + uri + '\';');
         });
     });
 };
