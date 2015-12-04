a4e3225dcdfb161d7434055fb63662a107fb3781
Tom Clement <mr@tomclement.nl>
2015-12-04 16:06:52 +0100

    Fixed issue in ObjectHelper causing getEnumerableProperties to return an empty object. Added regression test for the fixed issue

--- config.js
+++ config.js
@@ -1,16 +1,25 @@
 System.config({
-  "baseURL": "/",
-  "defaultJSExtensions": true,
-  "transpiler": "traceur",
-  "paths": {
-    "*": "src/*",
+  baseURL: "./",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
+    "stage": 0,
+    "optional": [
+      "runtime",
+      "es7.decorators",
+      "es7.asyncFunctions",
+      "optimisation.modules.system"
+    ]
+  },
+  paths: {
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
-  }
-});
+  },
 
-System.config({
-  "map": {
+  map: {
+    "babel": "npm:babel-core@5.8.34",
+    "babel-runtime": "npm:babel-runtime@5.8.34",
+    "core-js": "npm:core-js@1.2.6",
     "di": "github:bizboard/di.js@master",
     "fs": "github:jspm/nodelibs-fs@0.1.2",
     "lodash": "npm:lodash@3.10.1",
@@ -32,6 +41,15 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
+    "npm:babel-runtime@5.8.34": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:core-js@1.2.6": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
@@ -50,4 +68,3 @@ System.config({
     }
   }
 });
-
