96ba8ffc715afdcbfc1d64ddab3ab7b5d33677dd
Tom Clement <mr@tomclement.nl>
2016-02-02 15:29:38 +0100

    Updated dependencies

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ System.config({
   },
 
   map: {
-    "babel": "npm:babel-core@5.8.34",
-    "babel-runtime": "npm:babel-runtime@5.8.34",
+    "babel": "npm:babel-core@5.8.35",
+    "babel-runtime": "npm:babel-runtime@5.8.35",
     "bowser": "npm:bowser@1.0.0",
     "core-js": "npm:core-js@1.2.6",
     "di": "github:bizboard/di.js@master",
     "fs": "github:jspm/nodelibs-fs@0.1.2",
-    "lodash": "npm:lodash@3.10.1",
+    "lodash": "npm:lodash@4.2.0",
     "path": "github:jspm/nodelibs-path@0.1.0",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
@@ -40,7 +40,7 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:babel-runtime@5.8.34": {
+    "npm:babel-runtime@5.8.35": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:core-js@1.2.6": {
@@ -52,7 +52,7 @@ System.config({
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
-    "npm:lodash@3.10.1": {
+    "npm:lodash@4.2.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:path-browserify@0.0.0": {
